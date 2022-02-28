0c52b3017f1f3cbb156e2021bd3bbfc7525a2ae9
blagoi <dimitrovblagoi@gmail.ckm>
2022-02-28 18:52:25 +0300

    added mfcc

---
+++ pre_processing/audio/audio_features.py
@@ -0,0 +1,15 @@
+import librosa.display
+import matplotlib.pyplot as plt
+
+
+def get_mfccs(source):
+    x, sr = librosa.load(source)
+    return librosa.feature.mfcc(x, sr=sr)
+
+
+def get_mfccs_as_plot(source):
+    x, sr = librosa.load(source)
+    mfccs = librosa.feature.mfcc(x, sr=sr)
+    plt.figure(figsize=(15, 7))
+    librosa.display.specshow(mfccs, sr=sr, x_axis='time')
+    plt.show()
