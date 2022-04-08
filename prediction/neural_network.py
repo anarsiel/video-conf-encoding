b7643f872a014e61a2210885cb3611ec580e0ea1
blagoi <dimitrovblagoi@gmail.ckm>
2022-04-08 16:59:06 +0300

    load_dataset() done

--- prediction/neural_network.py
+++ prediction/neural_network.py
@@ -1,53 +1,88 @@
-#
-# https://python.ivan-shamaev.ru/keras-tutorial-beginner-guide-to-deep-learning-in-python/
-# правки, см. сюда: https://github.com/Rayhane-mamah/Tacotron-2/issues/73
-#
-
 import numpy as np
+import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 from keras.datasets import mnist
 from matplotlib import pyplot as plt
-np.random.seed(123)  # для воспроизводимости
+from PIL import Image
+import os
+np.random.seed(123)
 
-# Загрузка предварительно перемешанных данных MNIST в наборы trains и tests
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# Преобразование набора данных из формы (n, ширина, высота) в (n, глубина, ширина, высота)
-X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
-X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+def load_dataset(source, for_train=0.8):
+    frames_dir = f"{source}/frames"
+    mfccs_dir = f"{source}/mfccs"
 
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
+    files = [file for file in os.listdir(mfccs_dir) if check_mfcc_file(file)]
 
-# Нормализация значений данных в диапазоне [0, 1]
-X_train /= 255
-X_test /= 255
+    datasetX = np.zeros(shape=(len(files), 9860), dtype=float)  # 25 - число кадров, 9000 = 50 * 60 * 3 - кадр
+    datasetY = np.zeros(shape=(len(files), 216000), dtype=float)  # 24 * 9000
+    for idx, file in enumerate(sorted(files)):
+        filename = file.split('.')[0]
 
-# Преобразование одномерных массивов классов в 10-мерные матрицы классов
-Y_train = np_utils.to_categorical(y_train, 10)
-Y_test = np_utils.to_categorical(y_test, 10)
+        mfccs = load_mfccs(f"{mfccs_dir}/{file}")
+        frames = load_frames(f"{frames_dir}/{filename}")
 
-#
-# Model
-#
+        mfccs = mfccs.reshape(860)  # 9860 = 20*43 + 9000, 20 количество mfcc, 43 - количество mfcc на одну секунду видео
 
-model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1), data_format='channels_last'))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(10, activation='softmax'))
+        X, Y = np.concatenate((mfccs, frames[0])), frames[1:].reshape(216000)
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+        datasetX[idx] = X
+        datasetY[idx] = Y
 
-model.fit(X_train, Y_train, batch_size=32, epochs=10, verbose=1)
-score = model.evaluate(X_test, Y_test, verbose=0)
-print(score)
+    train_size = int(len(files) * for_train)
+
+    trainX, testX = datasetX[:train_size], datasetX[train_size:]
+    trainY, testY = datasetY[:train_size], datasetY[train_size:]
+
+    return trainX, trainY, testX, testY
+
+
+def load_mfccs(file):
+    return np.loadtxt(file)
+
+
+def load_frames(source_dir):
+    files = [file for file in os.listdir(source_dir) if check_frame_file(file)]
+
+    all_frames = np.zeros(shape=(25, 9000))
+    for idx, file in enumerate(files):
+
+        img = Image.open(f"{source_dir}/{file}")
+        image = np.array(img)
+        image = image.reshape(9000)
+
+        all_frames[idx] = image
+
+    return all_frames
+
+
+def save_image(image, name="tmp.jpg"):
+    ax = plt.subplot(3, 3, 1)
+    plt.imshow(image.astype("uint8"))
+    plt.axis("off")
+    plt.savefig(name)
+
+
+def read_image(source, image_shape=(50, 60)):
+    image = tf.io.read_file(source)
+    image = tf.io.decode_jpeg(image, channels=3)
+    image = tf.reshape(image, (1, image_shape[0] * image_shape[1], 3))  # (1, 3000, 3)
+    image = tf.cast(image, tf.float32)
+    image = tf.cast(image / 255., tf.float32)
+    return image
+
+
+def check_mfcc_file(file):
+    elements = file.split('.')
+    return len(elements) == 2 and elements[-1] == 'csv'
+
+
+def check_frame_file(file):
+    elements = file.split('.')
+    return len(elements) == 2 and elements[-1] == 'jpg'
+
+
+load_dataset("../dataset")
