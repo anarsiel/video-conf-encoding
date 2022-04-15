6c4d4b505e319ec268c4f1bede4912e87fd77e55
blagoi <dimitrovblagoi@gmail.ckm>
2022-04-15 19:49:24 +0300

    big datasets preprocessing fix

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from pre_processing.preprocess import create_dataset
+from preprocessing.preprocess import create_dataset
 
-# create_dataset("s1", "dataset")
-create_dataset("GRID_sample/video", "dataset_small")
+success, broken_files = 0, []
+for person_id in range(1, 0):  # 35):
+    if person_id == 21:
+        continue
+
+    # s, bf = create_dataset(f"GRID/s{person_id}", "grid_dataset")
+
+    success += s
+    broken_files += bf
+
+    print(f"s{person_id} preprocessed")
+
+print(f"\nTotal files preprocessed: {success}. \nBroken files: {len(broken_files)}.")
+
+f = open("broken_files.txt", "w")
+f.write('\n'.join(broken_files))
+f.close()
