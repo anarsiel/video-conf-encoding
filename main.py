e66f6e23979a2d2809f1f3db5ca9237c87d42525
blagoi <dimitrovblagoi@gmail.ckm>
2022-05-07 19:53:55 +0300

    beauty fix

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# from predicting.predicting import predict
 from PIL import Image
 from preprocessing.preprocess import create_dataset
 from predicting.generator import Generator
@@ -6,26 +5,10 @@ import numpy as np
 import cv2
 import os
 
-# s, bf = create_dataset(f"dataset_small", "alksd")
-
-# image = Image.open('predicting/face2.jpeg')
-# image.thumbnail((360, 360))
-# image.save('image_400.jpg')
-#
-# # predict("predicting/face.jpg", "predicting/University_ITMO.mp3", model_source="predicting/weights-2.h5", only_weights=True)
-# predict(
-#     "predicting/face.jpg",
-#     "predicting/University_ITMO.mp3",
-#     model_source="predicting/TimeDistributedModel/TimeDistributedModel",
-#     only_weights=True
-# )
-
 
 def change_res(source):
     os.system(f'ffmpeg -y -i {source} -vf scale=280:-1 r_{source}')
 
-
-# change_res("aa.mp4")
 
 generator = Generator(
     weights_path="predicting/current_best.hdf5",
@@ -34,13 +17,4 @@ generator = Generator(
     std_au_path="predicting/STD_AU.csv"
 )
 
-# generator.generate_video("resources/videos/sgwp8n.mpg", 5)
 generator.generate_video("gg.mp4", 5)
-
-# for idx, frame in enumerate(frames):
-#     # image = Image.fromarray(np.uint8(frame))
-#     # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-#     # image.save(f"out/{idx}.jpg")
-#
-#     # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#     cv2.imwrite(f"out/{idx}.jpg", frame)
